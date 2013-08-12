856a9e20aa3faa33b1eaf49431897b1093af6345
Frederik Nakstad <nakstad@gmail.com>
2013-08-12 12:16:07 +0200

    Scaffold basic static server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express =       require('express')
+    , http =        require('http')
+    , path =        require('path');
+
+var app = express();
+
+app.use(express.logger('dev'))
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'client/views/')));
+
+app.set('port', process.env.PORT || 8000);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
