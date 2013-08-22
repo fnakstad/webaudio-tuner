9ca426f565916b44288c8640f1cf6f199bf72bb9
Frederik Nakstad <nakstad@gmail.com>
2013-08-22 23:36:09 +0200

    Use LESS instead of plain old CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,23 @@
 module.exports = function(grunt) {
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-less');
 
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["client/css"]
+                },
+                files: {
+                    "client/css/app.css": "client/css/app.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["client/css"],
+                    yuicompress: true
+                }
+            }
+        }
+    });
 };
