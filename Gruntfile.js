46a4880b86f46a7aa72b3e67a88b4a0c4259e834
Frederik Nakstad <nakstad@gmail.com>
2013-08-23 14:33:19 +0200

    Added new Grunt task, dev, to run when developing the application.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,48 @@
 module.exports = function(grunt) {
     // Load plugins
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
-        less: {
-            development: {
-                options: {
-                    paths: ["client/css"]
-                },
-                files: {
-                    "client/css/app.css": "client/css/app.less"
-                }
-            },
-            production: {
-                options: {
-                    paths: ["client/css"],
-                    yuicompress: true
-                }
+        less: lessConfig,
+
+        watch: {
+            scripts: {
+                files: ['client/css/*.less'],
+                tasks: ['less:development']
             }
         }
     });
+
+    // Register custom tasks
+    grunt.registerTask('nodemon', function() {
+        var nodemon = grunt.util.spawn({
+            cmd: 'nodemon',
+            args: ['server.js']
+        });
+        nodemon.stdout.pipe(process.stdout);
+        nodemon.stderr.pipe(process.stderr);
+    });
+
+    grunt.registerTask('dev', ['nodemon', 'watch']);
+};
+
+var lessConfig = {
+    development: {
+        options: {
+            paths: ["client/css"]
+        },
+        files: {
+            "client/css/app.css": "client/css/app.less"
+        }
+    },
+    production: {
+        options: {
+            paths: ["client/css"],
+            yuicompress: true
+        },
+        files: {
+            "client/css/app.css": "client/css/app.less"
+        }
+    }
 };
